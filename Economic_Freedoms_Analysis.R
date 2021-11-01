61b198408cf4ca5eff646774c3680067814d8a33
matthewdmdolan <matthewdmdolan@gmail.com>
2021-11-01 00:12:11 +0000

    most recent commit

--- Economic_Freedoms_Analysis.R
+++ Economic_Freedoms_Analysis.R
@@ -11,6 +11,9 @@ library('cowplot')
 library('jtools')
 library('car')
 library('MASS')
+library('caTools')
+library('dplyr')
+library('gather')
 
 options(warn = -1)
 
@@ -113,7 +116,149 @@ p3 <- plot_geo(data1) %>%
 htmlwidgets::saveWidget(p3, "p3.html")
 display_html('<iframe src="p3.html" width=100% height=450></iframe>')
 
+#augmenting data for regression analysis
+data2 <- data1
+colnames(data2) <- paste("Sub", colnames(data), sep = "_")
+colnames(data2)[1] <- "year"
+colnames(data2)[2] <- "ISO_codes"
+colnames(data2)[3] <- "countries"
+colnames(data2)[4] <- "Economic_Freedom"
+colnames(data2)[5] <- "rank"
+colnames(data2)[6] <- "quartile"
+
+#sense checking data to ensure in correct shape for regression 
+head(data2)
+
+#removing year, countries, ISO codes, quartile and rank and removing missing values
+data_model <- data2[, -c(1,2,3,5,6)]
+data_model <- na.omit(data_model)
+
+#sense checking new data set
+str(data_model)
+head(data_model)
+
+#splitting data set into test/train
+set.seed(100)
+indices = sample.split(data_model$Economic_Freedom, SplitRatio = 0.7)
+train = data_model[indices,]
+test = data_model[!(indices),]
+
+#both the multiple R-squared and adjusted R-squared are 1, so it's evident that multicollinear variables are present in the data
+model_1 <-lm(Economic_Freedom~.,data=train)
+summary(model_1)
+
+#Step AIC function used to remove the variables
+step <- stepAIC(model_1, direction="both")
+step
+
+#multicollinear variables  still exist in our data based on our r squared values. therefore we will use VIF(variance inflation factor) to get rid of redundant predictors/variables with  high multicollinearity dependencies
+model_2 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1b_transfers + Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_2_property_rights + Sub_3a_money_growth + 
+                Sub_3b_std_inflation + Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4b_regulatory_trade_barriers + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg + Sub_5c_business_reg, data = train)
+
+summary(model_2)
+
+#property_rights has high vif, so in the next model property_rights will be removed
+sort(vif(model_2))
+
+#rerunning model without property_rights based on vif results
+model_3 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1b_transfers + Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_3a_money_growth + 
+                Sub_3b_std_inflation + Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4b_regulatory_trade_barriers + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg + Sub_5c_business_reg, data = train)
+
+summary(model_3)
+
+#the Sub_5c_business_reg evidences a high VIF score, meaning it will be removed in the next model
+sort(vif(model_3))
+
+#re-running the model without Sub_5c_business_reg due to high VIF scored evidence
+model_4 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1b_transfers + Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_3a_money_growth + 
+                Sub_3b_std_inflation + Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4b_regulatory_trade_barriers + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg, data = train)
+
+summary(model_4)
 
 
+# Sub_4b_regulatory_trade_barriers scored high VIF score so will be removed as a resul in next model
+sort(vif(model_4))
+
+#Removing  Sub_4b_regulatory_trade_barriers due to forementioned high VIF score
+model_5 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1b_transfers + Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_3a_money_growth + 
+                Sub_3b_std_inflation + Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg, data = train)
+
+summary(model_5)
+
+#Removing  Sub_3b_std_inflation evidences a high VIF score so will be omitted from next model and also removing variables with without significant p value scores
+sort(vif(model_5))
+
+#Reunning model without Sub_3b_std_inflation =  allindependent variables that have vif within 2.
+model_6 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1b_transfers + Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_3a_money_growth + 
+                Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg, data = train)
+
+summary(model_6)
+
+sort(vif(model_6))
+
+#Removing  Sub_1b_transfers due to high p-value 
+model_7 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_3a_money_growth + 
+                Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg, data = train)
+
+#Removing  Sub_3a_money_growth due to high p-value 
+summary(model_7)
+
+
+#re-running model with Sub_3a_money_growth due to high p-value 
+model_8 <- lm(formula = Economic_Freedom ~ Sub_1a_government_consumption + 
+                Sub_1c_gov_enterprises + Sub_1d_top_marg_tax_rate + 
+                Sub_2a_judicial_independence + Sub_3c_inflation + Sub_3d_freedom_own_foreign_currency + 
+                Sub_4a_tariffs + Sub_4c_black_market + 
+                Sub_4d_control_movement_capital_ppl + Sub_5a_credit_market_reg + 
+                Sub_5b_labor_market_reg, data = train)
+
+summary(model_8)
+
+#All independent variables have low p-values, now only 11 predictors are in the picture vs 30 predictors originally.
+Predict <- predict(model_8,test[,-1])
+test$test_efw <- Predict
+
+# here we test the r square between the model vs.the prediction of economic freedom vs the test data
+r <- cor(test$Economic_Freedom,test$test_efw)
+rsquared <- cor(test$Economic_Freedom,test$test_efw)^2
+rsquared
+
+#plotting the residuals
+options(repr.plot.width=6, repr.plot.height=6)
+par(mfrow = c(2, 2))
+plot(model_8)
+
+#using plot_summs function from  jtools package to compare the standardized coeeficients of predictors. Predictors that have highest estimate of standardized coefficient in the plot below, offer the most analytical value.
+plot_summs(model_8, scale = TRUE, plot.distributions = TRUE)
 
 
